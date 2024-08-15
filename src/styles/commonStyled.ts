8dff7ba669870d97b3823cec1dcd1ce9cf0e6cc6
Oleksandr-Berd <alex.berd86@gmail.com>
2024-08-15 19:41:01 +0300

    mob is done

--- src/styles/commonStyled.ts
+++ src/styles/commonStyled.ts
@@ -58,6 +58,14 @@ export const AuthorCon = styled.div`
   display: flex;
   justify-content: space-between;
 
+  padding-top: 16px;
+  padding-right: 32px;
+  padding-bottom: 16px;
+  padding-left: 32px;
+
+  border-bottom-right-radius: 8px;
+  border-bottom-left-radius: 8px;
+
   & > div {
     display: flex;
     justify-content: space-between;
@@ -78,21 +86,45 @@ export const AuthorTitle = styled.h4`
 export const Button = styled.button`
   border: none;
   background-color: transparent;
+
 `;
 
 export const ShareCommonCon = styled.div`
-display: flex;
-flex-direction: row;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding-top: 16px;
+  padding-right: 32px;
+  padding-bottom: 16px;
+  padding-left: 32px;
 
   background-color: ${({ theme }) => theme.color.title};
+
+  border-bottom-right-radius: 8px;
+  border-bottom-left-radius: 8px;
+
+box-shadow: 4px 10px 21px 0px rgba(194,167,194,1);
 `;
 
-
 export const ShareTitle = styled.h5`
-color: ${({theme})=> theme.color.layout}
-`
+text-transform: uppercase;
+
+font-family: "ManRopeMed";
+letter-spacing: 5px;
+line-height: 1.5;
+font-size: 13px;
+
+  color: ${({ theme }) => theme.color.layout};
+`;
 
 export const IconsList = styled.ul`
-display: flex;
-flex-direction: row;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+
+& > li:not(:last-child){
+    margin-right: 16px;
+}
+`;
